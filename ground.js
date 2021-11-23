bbdaf92f7bb98ff7dd341590104dcb2ab3377342
Ishita2006 <82094541+Ishita2006@users.noreply.github.com>
2021-11-23 16:59:39 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,20 @@
+class Ground{
+    constructor(x,y,width,hieght){
+       var options = {
+          isStatic: true,
+        }
+
+        this.body = Bodies.rectangle(x,y,width,hieght,options)
+        this.width = width
+        this.hieght = hieght 
+
+        World.add(world,this.body)
+    }
+    display(){
+        var pos = this.body.position
+        rectMode(CENTER)
+        fill(235,144,89)
+        rect(pos.x,pos.y,this.width,this.hieght)
+
+    }
+}
